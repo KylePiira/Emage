d6fc9c37fe149b963c15d0d9d0f5a5e32464741c
Kyle Piira <kylepiira@gmail.com>
2017-02-02 19:21:07 -0500

    Outputs to image

--- emage.py
+++ emage.py
@@ -1,9 +1,20 @@
 # Emage (Encrypt Image Project)
-# v0.1 Alpha - Jan 29, 2017
+# v0.2 Alpha - Jan 29, 2017
 # Copyright (C) 2016-2017 Kyle Piira
 
-import os, hashlib, getpass, binascii, random, math, simplecrypt
+import os, hashlib, getpass, binascii, random, simplecrypt
 from PIL import Image
+
+# Convert Hex codes to RGB
+def hexToRGB(value):
+    # Return (red, green, blue) for the color given as #rrggbb.
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+# Convert RGB to Hex codes
+def rgbToHex(red, green, blue):
+    # Return color as #rrggbb for the given color values
+    return '%02x%02x%02x' % (red, green, blue)
 
 def openImage(imgPath):
     img = Image.open(imgPath)
@@ -19,24 +30,51 @@ def openImage(imgPath):
 
     return pixels, (width, height), img
 
-def embedImage(imgPath, salt, message):
-    pixels, shape, img = openImage(imgPath)
-    embedCode = str(salt.decode('utf_8')) + str(message.decode('utf_8'))
+def embedImage(imgPath, img, pixels, salt, message):
+    # '000000' is the devider between salt and message.
+    embedCode = str(salt.decode('utf_8')) + '000000' + str(message.decode('utf_8'))
     n = 6 # Number of chars in each 'byte' of embed code.
     embedBytes = [embedCode[i:i+n] for i in range(0, len(embedCode), n)]
 
-    def bytesProcess(bytes):
-        bytes[-1]
+    # Make last byte a full 6 chars
+    embedBytes[-1] = embedBytes[-1].zfill(6)
 
+    for i in range(len(embedBytes)):
+        pixels[i] = hexToRGB(embedBytes[i])
+
+    width, height = img.size # Store the original size of Image
+    pix = img.load()
+    pixelNum = 0
+    for row in range(height):
+        for pixel in range(width):
+            # Creates a list of each pixel in image
+            pix[pixel, row] = pixels[pixelNum]
+            pixelNum += 1
+
+    img.save(imgPath)
 # Generate a key from current seed
 def genKeyFromHash(length, hash):
     random.seed(hash)
     return ''.join(["%s" % random.randint(0, 9) for num in range(0, length)])
 
+# Generate hash from password
+def passHash(algorthm, password, salt, iters):
+    return binascii.hexlify(
+                hashlib.pbkdf2_hmac(
+                                algorthm,
+                                password.encode(encoding='utf_8'),
+                                salt,
+                                iters
+                            )
+                        )
+
 def encrypt(message, password, iters = 1000000, algorthm = 'sha512'):
-    salt = binascii.hexlify(os.urandom(64)) # Generate Cryptographic Hex Salt
+    # Generate Cryptographic Hex Salt
+    # Salt length must be a multiple of
+    # 6 so that it can fit in Hex codes
+    salt = binascii.hexlify(os.urandom(66))
     # Hash the password using above salt.
-    passwordHash = binascii.hexlify(hashlib.pbkdf2_hmac(algorthm, password.encode(encoding='utf_8'), salt, iters))
+    passwordHash = passHash(algorthm, password, salt, iters)
 
     # Encrypt Message
     def messageEncrypt(hash, message):
@@ -50,7 +88,7 @@ def encrypt(message, password, iters = 1000000, algorthm = 'sha512'):
 
 def decrypt(message, password, salt, iters = 1000000, algorthm = 'sha512'):
     # Generate hash using password and salt
-    passwordHash = binascii.hexlify(hashlib.pbkdf2_hmac(algorthm, password.encode(encoding='utf_8'), salt, iters))
+    passwordHash = passHash(algorthm, password, salt, iters)
 
     # Decrypt Message
     def messageDecrypt(hash, message):
@@ -66,4 +104,5 @@ def decrypt(message, password, salt, iters = 1000000, algorthm = 'sha512'):
 message = input('Message: ')
 password = getpass.getpass(prompt='Password: ')
 salt, passHash, message = encrypt(message, password)
-embedImage('Image.png', salt, message)
+pixels, (width, height), img = openImage('Image.png')
+embedImage('Image.png', img, pixels, salt, message)
