972bc5d27dfb9312b5b37cea23807a6c5d437cdf
Kyle Piira <kylepiira@gmail.com>
2017-02-05 11:18:08 -0500

    Merged emage.py and __init__

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 setup(
   name = 'emage',
   packages = ['emage'], # this must be the same as the name above
-  version = '0.5',
+  version = 'v0.5.1',
   description = 'A library for encrypting and decrypting data into images.',
   author = 'Kyle Piira',
   author_email = 'contact@kylepiira.com',
   url = 'https://github.com/KylePiira/Emage', # use the URL to the github repo
-  download_url = 'https://github.com/KylePiira/Emage/tarball/0.5',
+  download_url = 'https://github.com/KylePiira/Emage/archive/v0.5.tar.gz',
   keywords = ['emage', 'encryption', 'decryption'], # arbitrary keywords
+  license = 'GNUv3',
   classifiers = [],
+  install_requires=['simplecrypt','pillow','random','binascii','hashlib'],
 )
