9c5a9b539ed1d9c571f8213ecc7ad9b3d679074f
Kyle Piira <kylepiira@gmail.com>
2017-02-05 11:26:03 -0500

    Fixed requirements

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'emage',
   packages = ['emage'], # this must be the same as the name above
-  version = 'v0.5.1',
+  version = 'v0.5.2b',
   description = 'A library for encrypting and decrypting data into images.',
   author = 'Kyle Piira',
   author_email = 'contact@kylepiira.com',
   url = 'https://github.com/KylePiira/Emage', # use the URL to the github repo
-  download_url = 'https://github.com/KylePiira/Emage/archive/v0.5.tar.gz',
+  download_url = 'https://github.com/KylePiira/Emage/archive/v0.5.2.tar.gz',
   keywords = ['emage', 'encryption', 'decryption'], # arbitrary keywords
   license = 'GNUv3',
   classifiers = [],
-  install_requires=['simplecrypt','pillow','random','binascii','hashlib'],
+  install_requires=['simple-crypt','pillow'],
 )
